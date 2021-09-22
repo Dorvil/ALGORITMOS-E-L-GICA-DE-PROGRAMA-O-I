5686371b2eaaf63ee67dc059dd00baed8954af91
Davidson Dorvil <86333511+Dorvil@users.noreply.github.com>
2021-09-22 19:43:26 -0300

    Update exercicio2.cpp

--- Tabalho2Davidson/exercicio2.cpp
+++ Tabalho2Davidson/exercicio2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-		/*
+		
 		int num = 100;
 		
 		while ( num >=1){
@@ -9,22 +9,22 @@ int main(){
 			cout << num << "\n";
 			}
 	
-	*/	
-  /* 
+		
+ 
 		int num = 100;
 		do {
 			num--;
 			cout << num <<"\n";
 				
 		}while( num >= 1 );
-*/		
 		
-/*		
+		
+		
 	 for(int num = 100;  num >= 1; num--){
 		cout << num << "\n";
 		
 	}
-*/
+
 		
 	//comando em tecla
 	system ("pause");
